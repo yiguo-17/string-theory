d3d0f5aff6787d91e0bdf9801755971718ae4294
yiguo-17 <yiguo.qu@codeimmersives.com>
2020-06-30 13:05:40 -0400

    edit the functions, first draft.

--- main.js
+++ main.js
@@ -8,6 +8,115 @@ const indexedChars = function(str){
   }
   return result
 
+const exclaim = function(str){
+  let result = '';
+  for(i=0; i<str.length;i++){
+    if(str[i]==='.'||str[i]==='?'){
+      result+= '!';
+    }else{result+= str[i];}
+  }
+  return result;
+}
+const repeatIt = function(str, num){
+  let result = '';
+  for(i=0; i<num; i++){
+    result+= str;
+  }
+  return result;
+}
+const truncate = function(str){
+  let result = '';
+  for (i=0; i<15; i++){
+    result+= str[i];
+  }
+  result+= '...'
+  return result;
+}
+const ciEmailify = function(str){
+  let result = '';
+  const name = str.toLowerCase();
+  for(i=0; i<name.length;i++){
+    if(name[i]===' '){
+      result+= '.';
+    }else{result+= name[i];}
+  }
+  result+= '@codeimmersives.com'
+  return result
+}
+
+const reverse = function(str){
+  let result = '';
+  for(i=0; i<str.length;i++){
+    result+= str[str.length-1];
+  }
+  return result;
+}
+const voewl = 'AEIOU';
+const onlyVowels = function(str){
+  const ucs = str.toUpperCase();
+  let result = '';
+  for(i=0;i<str.length;i++){
+    if(voewl.includes(usc[i])){
+      result+= str[i];
+    }
+  }
+  return result;
+}
+const numberedChars = function(str){
+  let result = '';
+  for(i= 0; i<str.length; i++){
+    result +='('+(i+1)+')' +str[i];
+  }
+  return result
+}
+
+const crazyCase = function(str){
+  let result = '';
+  for (i=0; i<str.length;i++){
+    if(i%2 ===0){result+= str[i].toLowerCase();}
+    else{result+=str[i].toUpperCase();}
+  }
+  return result;
+}
+const titleCase = function(str){
+  let result = '';
+  result+=str[0].toUpperCase();
+  for(i=1; i<str.length;i++){
+    if(str[i-1]===' '){result+= str[i].toUpperCase()}
+    else{result+= str[i].toLowerCase()}
+  }
+  return result;
+}
+const camelCase = function(str){
+  let result = '';
+  result+=str[0].toLowerCase();
+  for(i=1; i<str.length;i++){
+    if(str[i-1]===' '){result+= str[i].toUpperCase()}
+    else{result+= str[i].toLowerCase()}
+  }
+  let camelStr='';
+  for(i=0;i<result.length;i++){
+    if(result[i]===' '){camelStr = camelStr;}
+    else{camelStr+= result[i];}
+  }
+  return camelStr;
+}
+const crazyCase2ReturnOfCrazyCase = function(str){
+  let result ='';
+  const ucs = 'ABCDEFGHIJKLMNOPQRESTUVWXYZ';
+  const lcs = ucs.toLowerCase();
+  result+= str[0].toLowerCase();
+  for(i=1;i,str.length;i++){
+    if(ucs.includes(str[i-1])){result+= str[i].toLowerCase()}
+    if(lcs.includes(str[i-1])){result+= str[i].toUpperCase()}
+    if(str[i-1]===' ' &&ucs.includes(str[i-2])){result+= str[i].toLowerCase()}
+    if(str[i-1]===' ' &&lcs.includes(str[i-2])){result+= str[i].toUpperCase()}
+    if(str[i]===' '){result+=str[i]}
+  }
+  return result
+
+}
+
 
 
 
